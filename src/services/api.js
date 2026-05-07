44ca18f2d4355294f9d667255e61d143ad0c1ab8
Terry Ntuli <meriusterry>
2026-05-07 18:56:34 +0200

    second  commit with some new fixes

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://18.132.200.126:5001'//'http://localhost:5001/api';  
+//const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
     baseURL: API_URL,
